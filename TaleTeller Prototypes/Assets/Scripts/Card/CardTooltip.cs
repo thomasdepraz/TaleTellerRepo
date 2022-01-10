3ac95b8c326fbddc35abbe4c33c25154c1b4e262
Thomas Depraz <t.deprazdepland@gmail.com>
2022-01-10 11:42:38 +0100

    fix : fixed new base deck cards

--- TaleTeller Prototypes/Assets/Scripts/Card/CardTooltip.cs
+++ TaleTeller Prototypes/Assets/Scripts/Card/CardTooltip.cs
@@ -11,9 +11,11 @@ public class CardTooltip : MonoBehaviour
     public TextMeshProUGUI tooltipTextVisible;
     private string text;
     private float tweeningValue;
+    private bool hovered;
 
     public void AppearTooltip(string key, int direction, float delay)
     {
+        hovered = true;
         //Load text
         text = $"{UtilityClass.ToBold(key)}:\n {LocalizationManager.Instance.GetString(LocalizationManager.Instance.tooltipDictionary, key)} ";
         tooltipTextHidden.text = text;
@@ -38,36 +40,48 @@ public class CardTooltip : MonoBehaviour
             }
         }).setEaseInOutQuint().setOnStart(() => 
         {
-            gameObject.SetActive(false);
-            gameObject.SetActive(true);
+            if(!hovered)
+            {
+                LeanTween.cancel(gameObject);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+                gameObject.SetActive(true);
+            }
         });
     }
 
     public void HideTooltip(float delay)
     {
-        int direction = 0;
-        if (self.anchorMin.x < 0) direction = -1;
-        else direction = 1;
-
+        hovered = false;
         LeanTween.cancel(gameObject);
 
-        //Tween text
-        LeanTween.value(gameObject,tweeningValue, 0, 0.5f).setDelay(delay).setOnUpdate((float value) =>
-        {
-            if (direction > 0)
-            {
-                self.anchorMin = new Vector2(value, self.anchorMin.y);
-                self.anchorMax = new Vector2(value + 1, self.anchorMax.y);
-            }
-            else
-            {
-                self.anchorMin = new Vector2(-value, self.anchorMin.y);
-                self.anchorMax = new Vector2(1 - value, self.anchorMax.y);
-            }
-        }).setEaseInOutQuint().setOnComplete(()=> 
-        {
-            gameObject.SetActive(false);
-        });
+        ////int direction = 0;
+        //if (self.anchorMin.x < 0) direction = -1;
+        //else direction = 1;
+
+        gameObject.SetActive(false);
+        self.anchorMin = new Vector2(0, self.anchorMin.y);
+        self.anchorMax = new Vector2(1, self.anchorMax.y);
+
+        ////Tween text
+        //LeanTween.value(gameObject,tweeningValue, 0, 0.5f).setDelay(delay).setOnUpdate((float value) =>
+        //{
+        //    if (direction > 0)
+        //    {
+        //        self.anchorMin = new Vector2(value, self.anchorMin.y);
+        //        self.anchorMax = new Vector2(value + 1, self.anchorMax.y);
+        //    }
+        //    else
+        //    {
+        //        self.anchorMin = new Vector2(-value, self.anchorMin.y);
+        //        self.anchorMax = new Vector2(1 - value, self.anchorMax.y);
+        //    }
+        //}).setEaseInOutQuint().setOnComplete(()=> 
+        //{
+        //    gameObject.SetActive(false);
+        //});
 
         tweeningValue = 0;
     }
