d9d4d6beeb428d750f6b547c33c7d9a6fab8b11b
Orel <orelien.mary@gmail.com>
2022-01-19 19:43:49 +0100

    feat: Fin Inté PeacefulTown

--- TaleTeller Prototypes/Assets/Scripts/Card/PlotObjectives/ObjectiveTemplate/PlaceJunkObj.cs
+++ TaleTeller Prototypes/Assets/Scripts/Card/PlotObjectives/ObjectiveTemplate/PlaceJunkObj.cs
@@ -1,8 +1,9 @@
+using NaughtyAttributes;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using NaughtyAttributes;
 using System.Linq;
+using UnityEngine;
 
 public class PlaceJunkObj : JunkDrivenObj
 {
@@ -15,6 +16,27 @@ public class PlaceJunkObj : JunkDrivenObj
     public bool specifyNumbers;
     [ShowIf("specifyNumbers")]
     public List<int> numberToPlace = new List<int>();
+    public bool mustPlaceTypeCards;
+    [ShowIf("mustPlaceTypeCards")]
+    [SerializeField] List<TypeAmountRestrictionInfos> typeRestrictions = new List<TypeAmountRestrictionInfos>();
+
+    [Serializable]
+    struct TypeAmountRestrictionInfos
+    {
+        internal enum CardType { Character, Location, Object }
+        internal enum CharacterType { Peacefull, Agressive, Both }
+        internal enum RestrictionMethod { Maximum, Minimum, Exact }
+        bool ShowCharacterType => typeToHave == CardType.Character;
+
+        [SerializeField]
+        internal CardType typeToHave;
+        [SerializeField, ShowIf("ShowCharacterType"), AllowNesting]
+        internal CharacterType characterType;
+        [SerializeField]
+        internal RestrictionMethod restrictionMethod;
+        [SerializeField]
+        internal int count;
+    }
 
     public override void SubscribeUpdateStatus(PlotCard data)
     {
@@ -51,6 +73,110 @@ public class PlaceJunkObj : JunkDrivenObj
 
         var junkContainer = junkToTest.Select(j => j.currentContainer);
 
+        if(mustPlaceTypeCards)
+        {
+            var cardOnBoard = CardManager.Instance.board.slots
+                .Where(s => s.currentPlacedCard != null)
+                .Select(s => s.currentPlacedCard.data);
+
+            foreach (TypeAmountRestrictionInfos restriction in typeRestrictions)
+            {
+                switch (restriction.typeToHave)
+                {
+                    case TypeAmountRestrictionInfos.CardType.Character:
+
+                        var characterOnBoard = cardOnBoard.Where(d => d.cardType.GetType() == typeof(CharacterType));
+                        var characterTypes = characterOnBoard.Select(d => d.cardType);
+
+                        switch (restriction.characterType)
+                        {
+                            case TypeAmountRestrictionInfos.CharacterType.Agressive:
+
+                                int agressiveCount = 0;
+
+                                foreach (CardTypes type in characterTypes)
+                                {
+                                    var characType = type as CharacterType;
+                                    if (characType.behaviour == CharacterBehaviour.Agressive)
+                                        agressiveCount++;
+                                }
+
+                                if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Minimum
+                                    && agressiveCount < restriction.count)
+                                    return false;
+                                else if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Maximum
+                                    && agressiveCount > restriction.count)
+                                    return false;
+
+                                break;
+
+                            case TypeAmountRestrictionInfos.CharacterType.Peacefull:
+
+                                int peacefullCount = 0;
+
+                                foreach (CardTypes type in characterTypes)
+                                {
+                                    var characType = type as CharacterType;
+                                    if (characType.behaviour == CharacterBehaviour.Peaceful)
+                                        peacefullCount++;
+                                }
+
+                                if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Minimum
+                                    && peacefullCount < restriction.count)
+                                    return false;
+                                else if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Maximum
+                                    && peacefullCount > restriction.count)
+                                    return false;
+
+                                break;
+
+                            case TypeAmountRestrictionInfos.CharacterType.Both:
+                                if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Minimum
+                                    && characterOnBoard.Count() < restriction.count)
+                                    return false;
+                                else if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Maximum
+                                    && characterOnBoard.Count() > restriction.count)
+                                    return false;
+
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        break;
+
+                    case TypeAmountRestrictionInfos.CardType.Location:
+
+                        var locationOnBard = cardOnBoard.Where(d => d.cardType.GetType() == typeof(LocationType));
+
+                        if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Minimum
+                                   && locationOnBard.Count() < restriction.count)
+                            return false;
+                        else if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Maximum
+                            && locationOnBard.Count() > restriction.count)
+                            return false;
+
+                        break;
+
+                    case TypeAmountRestrictionInfos.CardType.Object:
+
+                        var objectOnBard = cardOnBoard.Where(d => d.cardType.GetType() == typeof(ObjectType));
+
+                        if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Minimum
+                                   && objectOnBard.Count() < restriction.count)
+                            return false;
+                        else if (restriction.restrictionMethod == TypeAmountRestrictionInfos.RestrictionMethod.Maximum
+                            && objectOnBard.Count() > restriction.count)
+                            return false;
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
 
         if(specifyNumbers)
         {
